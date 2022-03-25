c407b3ed559e28a2ef81992980b903d41e8d8263
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-03-25 13:46:34 -0700

    Was able to make the input and if working, It will take 5 inputs and give us one of 3 languages for suggestion.

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,16 @@ $(document).ready(function () {
     const type2 = $("input:radio[name=type2]:checked").val();
     const type3 = $("input:radio[name=type3]:checked").val();
     const type4 = $("input:radio[name=type4]:checked").val();
-    const type5 = $("input:radio[name=type5]:checked").parseInt();
+    const type5 = $("input:radio[name=type5]:checked").val();
 
     let result;
-    if (type1 === "qs1answer1" && type2 === "qs1answer1" && type3 === "qs2answer2" && type4 === "qs1answer1" && 50000 <= type5 <= 70000) {
+    if (type1 === "qsanswer1" && type2 === "qsanswer1" && type3 === "qsanswer2" && type4 === "qsanswer1" && 50000 <= type5 <= 70000) {
       result = "JavaScript"
 
+    } else if (type1 === "qsanswer2" && type2 === "qsanswer1" && type3 === "qsanswer1" && type4 === "qsanswer2" && 70000 < type5 <= 120000) {
+      result = "Python"
+    } else if (type1 === "qsanswer1" && type2 === "qsanswer2" && type3 === "qsanswer1" || "qsanswer2" && type4 === "qsanswer2" || "qsanswer1" && 30000 <= type5 < 50000) {
+      result = "Swift"
     }
 
     $("#output").text(result);
