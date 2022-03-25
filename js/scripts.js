6349498a0cf8a1c416f12e255f261b3cf5f7a013
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-03-25 11:55:30 -0700

    I am working on if statment and finding the way to take any number pass for one of answers.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $("form#quiz").submit(function (event) {
+    event.preventDefault();
+    const type1 = $("input:radio[name=type1]:checked").val();
+    const type2 = $("input:radio[name=type2]:checked").val();
+    const type3 = $("input:radio[name=type3]:checked").val();
+    const type4 = $("input:radio[name=type4]:checked").val();
+    const type5 = $("input:radio[name=type5]:checked").parseInt();
+
+    let result;
+    if (type1 === "qs1answer1" && type2 === "qs1answer1" && type3 === "qs2answer2" && type4 === "qs1answer1" && type5 === any) {
+      result = "JavaScript"
+
+    }
+    $("#output").text(result);
+  });
+});
